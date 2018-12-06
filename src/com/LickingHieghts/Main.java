09cf89bef2116cfdc81a5dc68c3420b7bf82eca7
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-12-06 08:44:33 -0500

    added a welcome screen

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-     int Days;
+       System.out.println("| Welcome to Lemonade Stand profit calculator  ");
 
-
+     int Days; // The number of days that have gone by.
+     int CupsOfLemondae;//this is for the amount of lemonade that was bought
+     int Cups; // this is the amount of cups you bought not directly tied in with cups of lemonade but kinda
 
     }
 }
