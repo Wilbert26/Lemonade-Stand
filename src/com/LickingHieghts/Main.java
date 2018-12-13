c33d386e4952195fc7d38fa84a3bf2a88f57a1a2
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-12-13 08:52:38 -0500

    Additions

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -3,12 +3,45 @@ package com.LickingHieghts;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-       System.out.println("| Welcome to Lemonade Stand profit calculator  ");
+
+        // write your code here
+     System.out.println("| Welcome to Lemonade Stand profit calculator  |");
 
      int Days; // The number of days that have gone by.
      int CupsOfLemondae;//this is for the amount of lemonade that was bought
      int Cups; // this is the amount of cups you bought not directly tied in with cups of lemonade but kinda
+
+     int Lemonade;
+     int sugar;
+     System.out.println("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
