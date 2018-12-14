4c17f360bb9d897971f4296d1d75fab70c48c507
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-12-14 08:11:56 -0500

    more

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -1,20 +1,24 @@
 package com.LickingHieghts;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // write your code here
      System.out.println("| Welcome to Lemonade Stand profit calculator  |");
-
+        Scanner keyboard = new Scanner(System.in);
      int Days; // The number of days that have gone by.
      int CupsOfLemondae;//this is for the amount of lemonade that was bought
      int Cups; // this is the amount of cups you bought not directly tied in with cups of lemonade but kinda
-
+     int price;
      int Lemonade;
      int sugar;
-     System.out.println("");
-
+     System.out.println("Please enter the amount of lemons you bought");
+     price = keyboard.nextInt();
+     System.out.println("Now enter the price per lemon");
+        price = keyboard.nextInt();
 
 
 
