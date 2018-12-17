c9251f69fe7936bcbe915eeb09ac48afedb379c4
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-12-17 08:32:58 -0500

    more

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -13,18 +13,26 @@ public class Main {
      int CupsOfLemondae;//this is for the amount of lemonade that was bought
      int Cups; // this is the amount of cups you bought not directly tied in with cups of lemonade but kinda
      int price;
+     int price1;
+     int price2;
+     int price3;
+     int price4;
+     int price5;
      int Lemonade;
      int sugar;
      System.out.println("Please enter the amount of lemons you bought");
      price = keyboard.nextInt();
      System.out.println("Now enter the price per lemon");
-        price = keyboard.nextInt();
+        price1 = keyboard.nextInt();
      System.out.println("The cost of sugar");
-        price = keyboard.nextInt();
+        price2 = keyboard.nextInt();
 System.out.println("How much of it did you buy");
-        price = keyboard.nextInt();
-        System.out.println("");
-
+        price3 = keyboard.nextInt();
+        System.out.println("Enter the number of days if none type 0");
+      Days = keyboard.nextInt();
+      System.out.println("Now enter any other extra supply amount");
+      price4 = keyboard.nextInt();
+      System.out.println("");
 
 
 
