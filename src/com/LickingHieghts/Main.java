05041b9a85adbfe657f4a88958bf63ae26479161
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-12-18 08:13:03 -0500

    I give up

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -18,8 +18,13 @@ public class Main {
      int price3;
      int price4;
      int price5;
+     int price6;
+     int price7;
+     int price8;
      int Lemonade;
      int sugar;
+     int Pricetotal;
+     int totalsupplies;
      System.out.println("Please enter the amount of lemons you bought");
      price = keyboard.nextInt();
      System.out.println("Now enter the price per lemon");
@@ -32,7 +37,18 @@ System.out.println("How much of it did you buy");
       Days = keyboard.nextInt();
       System.out.println("Now enter any other extra supply amount");
       price4 = keyboard.nextInt();
-      System.out.println("");
+      System.out.println("Now enter any other extra supply amount");
+        price5 = keyboard.nextInt();
+        System.out.println("Now enter any other extra supply amount");
+        price6 = keyboard.nextInt();
+        System.out.println("Now enter any other extra supply amount");
+        price7 = keyboard.nextInt();
+        System.out.println("Now enter any other extra supply amount");
+        price8 = keyboard.nextInt();
+System.out.println("What was the total amount you sold in $");
+        Pricetotal = keyboard.nextInt();
+        System.out.println("what was the total supply cost");
+        totalsupplies = keyboard.nextInt();
 
 
 
